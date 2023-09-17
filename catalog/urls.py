be923791268bf4030ddbf59d0f6c01b059122c7b
mgoo57 <142924730+mgoo57@users.noreply.github.com>
2023-09-17 17:19:43 -0500

    initial submission upload

---
+++ catalog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('book_list/', views.BookListView.as_view(), name='book_list'),
+    path('book/<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
+    path('authors/', views.author_list, name='author_list'),
+    path('author/<int:pk>/', views.author_detail, name='author_detail'),
+]
